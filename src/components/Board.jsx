9b2d29c8b1afd7dce6beaeae55f36d683b18a380
said <alsaghir.said@gmail.com>
2018-08-20 22:33:47 +0200

    til taking turns step

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,12 +2,32 @@ import React, {Component, Fragment} from 'react';
 import Square from './Square';
 
 class Board extends Component {
-  renderSquare(i) {
-    return <Square value={i} />;
+  constructor(props){
+    super(props);
+    this.state = {
+     squares: Array(9).fill(null),
+     xIsNext: true,
+   };
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+     squares[i] = this.state.xIsNext ? 'X' : 'O';
+     squares[i] = 'X';
+    this.setState({squares: squares,
+    xIsNext: !this.state.xIsNext,
+    });
+  }
+
+    renderSquare(i) {
+     return <Square value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+    />;
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' +
+     (this.state.xIsNext ? 'X' : 'O');
+
 
     return (
       <Fragment>
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 
-const Square = ({value}) => (
-
-      <button className="square">
-        {value}
-      </button>
-
-);
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
 export default Square;
